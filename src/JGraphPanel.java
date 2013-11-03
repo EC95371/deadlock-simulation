aca72d01f9fc05ec5a905f5b60abce912e827dc4
Anish Patel <anishpatel.0@gmail.com>
2013-11-03 01:05:58 -0400

    add JPanel graph GUI

---
+++ src/JGraphPanel.java
@@ -0,0 +1,109 @@
+import java.awt.geom.Rectangle2D;
+import javax.swing.*;
+
+import org.jgraph.*;
+import org.jgraph.graph.*;
+
+import org.jgrapht.*;
+import org.jgrapht.ext.*;
+//import org.jgrapht.graph.*;
+import org.jgrapht.graph.ListenableDirectedGraph;
+
+// resolve ambiguity
+import org.jgrapht.graph.DefaultEdge;
+
+public class JGraphPanel extends JPanel {
+    
+    private final JGraphModelAdapter<String, DefaultEdge> jgAdapter;
+    
+    public JGraphPanel() {
+        // create a JGraphT graph
+        ListenableDirectedGraph<String, DefaultEdge> graph = new ListenableDirectedGraph<>(DefaultEdge.class);
+
+        // create a visualization using JGraph, via an adapter
+        jgAdapter = new JGraphModelAdapter<>(graph);
+
+        JGraph jgraph = new JGraph(jgAdapter);
+        
+        this.add(jgraph);
+
+        String v1 = "v1";
+        String v2 = "v2";
+        String v3 = "v3";
+        String v4 = "v4";
+
+        // add some sample data (graph manipulated via JGraphT)
+        graph.addVertex(v1);
+        graph.addVertex(v2);
+        graph.addVertex(v3);
+        graph.addVertex(v4);
+
+        graph.addEdge(v1, v2);
+        graph.addEdge(v2, v3);
+        graph.addEdge(v3, v1);
+        graph.addEdge(v4, v3);
+
+        // position vertices nicely within JGraph component
+        positionVertexAt(v1, 130, 40);
+        positionVertexAt(v2, 60, 200);
+        positionVertexAt(v3, 310, 230);
+        positionVertexAt(v4, 380, 70);
+    }
+    
+    private void positionVertexAt(Object vertex, int x, int y)
+    {
+        DefaultGraphCell cell = jgAdapter.getVertexCell(vertex);
+        AttributeMap attr = cell.getAttributes();
+        Rectangle2D bounds = GraphConstants.getBounds(attr);
+
+        Rectangle2D newBounds =
+            new Rectangle2D.Double(
+                x,
+                y,
+                bounds.getWidth(),
+                bounds.getHeight());
+
+        GraphConstants.setBounds(attr, newBounds);
+
+        // TODO: Clean up generics once JGraph goes generic
+        AttributeMap cellAttr = new AttributeMap();
+        cellAttr.put(cell, attr);
+        jgAdapter.edit(cellAttr, null, null, null);
+    }
+    
+    public static void main(String [] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                //Create and set up the window.
+                JFrame frame = new JFrame("HelloWorldSwing");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                
+                JGraphPanel jgPanel = new JGraphPanel();
+                frame.getContentPane().add(jgPanel);
+                
+                try {
+                    // Set System L&F
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } 
+                catch (UnsupportedLookAndFeelException e) {
+                   // handle exception
+                }
+                catch (ClassNotFoundException e) {
+                   // handle exception
+                }
+                catch (InstantiationException e) {
+                   // handle exception
+                }
+                catch (IllegalAccessException e) {
+                   // handle exception
+                }
+
+                //Display the window.
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
+    }
+}
